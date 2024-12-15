bc14dae9de316078bce710c8b5b7b966e89794a9
wobbat <mail@wobbat.com>
2024-12-15 19:14:39 +0100

    updates

--- nvim.bak/.config/nvim/after/plugin/trouble.lua
+++
@@ -1,2 +0,0 @@
-vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>TroubleToggle<CR>', { noremap = true, silent = true })
-
--- nvim.bak/.config/nvim/lua/wobbat/init.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.termguicolors = true
-
-require("wobbat.keymap")
-require("wobbat.lazy")
-require("wobbat.basic")
-require("wobbat.commands")
-require("wobbat.netrw")
-require("wobbat.colors")
-require("wobbat.whopy")
